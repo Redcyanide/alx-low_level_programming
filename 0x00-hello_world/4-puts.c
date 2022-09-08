b7675743939cc77082f4d69ebbc1cb66239a2991
Olukoya David <olukoyadavid33@gmail.com>
2022-09-08 15:00:59 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: description for function main
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+puts("programming is like building a multilingual puzzle\n");
+return (0);
+}
