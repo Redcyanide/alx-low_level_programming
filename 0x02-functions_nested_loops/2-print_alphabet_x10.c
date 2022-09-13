7decbb0139ea3fdf6d9ebbbce72389970c04dfe4
Olukoya David <olukoyadavid33@gmail.com>
2022-09-13 16:10:39 -0700

    print lowercase letters ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - entry point
+*Description: a function to print the alphabet 10 times
+*Return:void
+*/
+
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+while (1 < 10)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
+}
