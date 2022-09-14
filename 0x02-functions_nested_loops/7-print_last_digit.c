63fe6a1303cf6543f2ac33ec841c730701b2b907
Olukoya David <olukoyadavid33@gmail.com>
2022-09-14 13:08:21 -0700

    print last digit of numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,10 @@ int print_last_digit(int n)
 {
 int a;
 a = n % 10;
+if (n <= 0)
 n = -n;
-if (n >= 0)
 _putchar(a + '0');
-else if (n <= 0)
-a = -a;
+else if (a <= 0)
 _putchar(a + '0');
 return (a);
 }
