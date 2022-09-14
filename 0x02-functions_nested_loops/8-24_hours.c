babcd39199e0ec1a7f402b4cfc6181552c61ca11
Olukoya David <olukoyadavid33@gmail.com>
2022-09-14 13:47:53 -0700

    print all minute of the day of jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*jack_bauer- prints every minute of the day of Jack Bauer
+*
+*Return: void
+*/
+void jack_bauer(void)
+{
+int i, j;
+i = 00;
+while (i < 24)
+{
+j = 00;
+while (j < 60)
+{
+_putchar((i : j) + '0');
+
+i++;
+j++;
+}
+return (void);
+}
+}
