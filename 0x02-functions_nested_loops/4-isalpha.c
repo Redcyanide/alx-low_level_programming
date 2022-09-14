7897a8b5d2622a7bd10eca62213c1e81aaae6100
Olukoya David <olukoyadavid33@gmail.com>
2022-09-13 17:05:08 -0700

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
 *@c: checks for alphabetic character
 *Return: 1 if c is a letter, lowercase or uppercase or uppercase, 0 otherwise
 */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
