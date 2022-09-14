3b06951fd34b7d634b40f29917fc3fae151350fa
Olukoya David <olukoyadavid33@gmail.com>
2022-09-14 15:10:46 -0700

    print 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*times_table - entry point(print times table)
+*
+*Return: (void)
+*/
+void times_table(void);
+{
+int a, b;
+
+  
+b = 9 * a;
+while (a >= 0)
+_putchar(b + '0');
+a++;
+}
