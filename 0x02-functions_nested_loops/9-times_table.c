d549570c4cf98b43c4a918a1492bcffb219dd148
Olukoya David <olukoyadavid33@gmail.com>
2022-09-14 15:44:04 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,8 +31,8 @@ else if (c >= 10)
 {
 _putchar(',');
 _putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
 }
 _putchar('\n');
